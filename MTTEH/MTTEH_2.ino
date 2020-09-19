04647b2ad61cefb15dd57ad8978a59dcaa925b3d
GGLinnk <gglinnk@protonmail.com>
2020-09-19 13:39:34 +0200

    Update MTTEH_(2, 3, 4).ino
    Assimilated elements ave been removed.
    Minor changes / Optimization.
    Renaming all movement function to new.
    Replace triggering block by trigger function.

--- MTTEH/MTTEH_2.ino
+++ MTTEH/MTTEH_2.ino
@@ -1,109 +1,38 @@
-#include <Arduino.h>
-#include <MTTEH.h>
+#include "MTTEH_Project.h"
 
-byte vitesse1 = 100;
-byte vitesse2 = 100;
-
-long randNumber;
-
-const byte TriggerPin= 10;
-const byte EchoPin = 11;
-
-const unsigned long MeasureTimeout = 25000UL;
-const float SoundSpeed = 340.0/1000;
-
-void setup(){
-  Serial.begin(115200);
-
-  pinMode(TriggerPin, OUTPUT);
-  digitalWrite(TriggerPin, LOW);
-  pinMode(EchoPin, INPUT);
-}
-
-void Arret(){
-  analogWrite(MAvD1, 0);
-  analogWrite(MArD1, 0);
-  analogWrite(MAvD2, 0);
-  analogWrite(MArD2, 0);
-  analogWrite(MAvG1, 0);
-  analogWrite(MArG1, 0);
-  analogWrite(MAvD2, 0);
-  analogWrite(MArD2, 0);
-  delay(20);
-}
-void Marcheavant(){
-  Arret();
-  analogWrite(MAvD1, vitesse1);
-  analogWrite(MArD1, vitesse1);
-  analogWrite(MAvD2, 0);
-  analogWrite(MArD2, 0);
-  analogWrite(MAvG1, vitesse2);
-  analogWrite(MArG1, vitesse2);
-  analogWrite(MAvD2, 0);
-  analogWrite(MArD2, 0);
-}
-
-void Tournerdroite(){
-  Arret();
-  analogWrite(MAvD1, 0);
-  analogWrite(MArD1, 0);
-  analogWrite(MAvD2, vitesse1);
-  analogWrite(MArD2, vitesse1);
-  analogWrite(MAvG1, vitesse2);
-  analogWrite(MArG1, vitesse2);
-  analogWrite(MAvD2, 0);
-  analogWrite(MArD2, 0);
-}
-void Tournergauche(){
-  Arret();
-  analogWrite(MAvD1, vitesse1);
-  analogWrite(MArD1, vitesse1);
-  analogWrite(MAvD2, 0);
-  analogWrite(MArD2, 0);
-  analogWrite(MAvG1, 0);
-  analogWrite(MArG1, 0);
-  analogWrite(MAvD2, vitesse2);
-  analogWrite(MArD2, vitesse2);
-}
+const unsigned long timeout_measure = 25000UL;
+const float sound_speed = 340.0/1000;
 
 void loop() {
-  Marcheavant();
-  digitalWrite(TriggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TriggerPin, LOW);
-  long measure = pulseIn(EchoPin, HIGH, MeasureTimeout);
-  float distance_c m= measure / 2.0 * SoundSpeed / 10.0;
-  delay(200);
-  if (distance_cm <= 50){
-    randNumber = random(2);
-    while (randNumber == 0 and distance_cm <= 50){
-      Arret();
-      Tournerdroite();
-      randNumber = random(5, 20);
-      delay(randNumber);
-      Arret();
-      digitalWrite(TriggerPin, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TriggerPin, LOW);
-      long measure = pulseIn(EchoPin, HIGH, MeasureTimeout);
-      float distance_cm = measure / 2.0 * SoundSpeed / 10.0;
-      delay(200);  
-    }
-    while (randNumber == 1 and distance_cm <= 50){
-      Arret();
-      Tournergauche();
-      randNumber = random(5, 20);
-      delay(randNumber);
-      Arret();
-      digitalWrite(TriggerPin, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TriggerPin, LOW);
-      long measure = pulseIn(EchoPin, HIGH, MeasureTimeout);
-      float distance_cm = measure / 2.0 * SoundSpeed / 10.0;
-      delay(200);
-    }
-  }
-  else{
-    Marcheavant();
-  }
+    static long randNumber = 0;
+
+    move_ahead();
+    trigger();
+    long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+    float distance_cm = measure / 2.0 * sound_speed / 10.0;
+    delay(200);
+    if (distance_cm <= 50){
+        randNumber = random(2);
+        while ((randNumber == 0) && (distance_cm <= 50)){
+            turn_right();
+            randNumber = random(5, 20);
+            delay(randNumber);
+            stop_moving();
+            trigger();
+            long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+            float distance_cm = measure / 2.0 * sound_speed / 10.0;
+            delay(200);  
+        }
+        while ((randNumber == 1) && (distance_cm <= 50)){
+            turn_left();
+            randNumber = random(5, 20);
+            delay(randNumber);
+            stop_moving();
+            trigger();
+            long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+            float distance_cm = measure / 2.0 * sound_speed / 10.0;
+            delay(200);
+        }
+    } else
+        move_ahead();
 }
--- MTTEH/MTTEH_3.ino
+++ MTTEH/MTTEH_3.ino
@@ -1,26 +1,14 @@
-#include <Arduino.h>
+#include "MTTEH_Project.h"
 
-const byte Triggerpin= 1;
-const byte Echopin = 0;
+const float timeout_measure = 000267F;
+const float light_speed = 299792458.0/1000;
 
-const float Measuretimeout = 000267F;
-const float LumSpeed = 299792458.0/1000;
-
-void setup(){
-  Serial.begin(115200);
-
-  pinMode(Triggerpin, OUTPUT);
-  digitalWrite(Triggerpin, LOW);
-  pinMode(Echopin, INPUT);
-}
 void loop(){
-  digitalWrite(Triggerpin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(Triggerpin, LOW);
-  long measure = pulseIn(Echopin, HIGH, Measuretimeout);
-  float distance_cm = measure / 2.0 * LumSpeed;
-  Serial.print("Distance: ");
-  Serial.print(distance_cm);
-  Serial.println("cm");
-  delay(200);
+    trigger();
+    long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+    float distance_cm = measure / 2.0 * light_speed;
+    Serial.print("Distance: ");
+    Serial.print(distance_cm);
+    Serial.println("cm");
+    delay(200);
 }
