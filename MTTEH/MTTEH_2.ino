58ef7ebda78ee8993847725cbfb2e4352e8e3a05
GGLinnk <gglinnk@protonmail.com>
2020-09-21 01:18:56 +0200

    Some optimisation and renaming

--- MTTEH/MTTEH_2.ino
+++ MTTEH/MTTEH_2.ino
@@ -1,4 +1,4 @@
-#include "MTTEH_Project.h"
+#include "MTTEH_Project.hpp"
 
 const unsigned long timeout_measure = 25000UL;
 const float sound_speed = 340.0 / 1000;
@@ -6,11 +6,13 @@ const float sound_speed = 340.0 / 1000;
 void loop()
 {
     static long randNumber = 0;
+    long measure = 0,
+    float distance_cm = 0.0;
 
     move_ahead();
     trigger();
-    long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
-    float distance_cm = measure / 2.0 * sound_speed / 10.0;
+    measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+    distance_cm = measure / 2.0 * sound_speed / 10.0;
     delay(200);
     if (distance_cm <= 50) {
         randNumber = random(2);
@@ -20,8 +22,8 @@ void loop()
             delay(randNumber);
             stop_moving();
             trigger();
-            long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
-            float distance_cm = measure / 2.0 * sound_speed / 10.0;
+            measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+            distance_cm = measure / 2.0 * sound_speed / 10.0;
             delay(200);
         }
         while ((randNumber == 1) && (distance_cm <= 50)) {
@@ -30,8 +32,8 @@ void loop()
             delay(randNumber);
             stop_moving();
             trigger();
-            long measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
-            float distance_cm = measure / 2.0 * sound_speed / 10.0;
+            measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
+            distance_cm = measure / 2.0 * sound_speed / 10.0;
             delay(200);
         }
     } else
