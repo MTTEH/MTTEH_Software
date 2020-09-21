b0bf61c0475fe96b75d73dbff4fed672e53ca435
GGLinnk <gglinnk@protonmail.com>
2020-09-21 06:30:51 +0200

    Full rework of MTTEH_2 : ready for assimilation

--- MTTEH/MTTEH_2.ino
+++ MTTEH/MTTEH_2.ino
@@ -1,41 +1,47 @@
 #include "MTTEH_Project.hpp"
 
-const unsigned long timeout_measure = 25000UL;
-const float sound_speed = 340.0 / 1000;
-
-void loop()
+long float get_distance(unsigned long wait_delay = 200)
 {
-    static long randNumber = 0;
-    long measure = 0,
-    float distance_cm = 0.0;
+    static bool print_error = 1;
+    unsigned long timeout_measure = 25000UL;
+    float sound_speed = 340.0 / 1000;
+    unsigned long measure = 0;
+    long float distance = 0.0;
 
-    move_ahead();
     trigger();
     measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
-    distance_cm = measure / 2.0 * sound_speed / 10.0;
-    delay(200);
-    if (distance_cm <= 50) {
-        randNumber = random(2);
-        while ((randNumber == 0) && (distance_cm <= 50)) {
-            turn_right();
-            randNumber = random(5, 20);
-            delay(randNumber);
-            stop_moving();
-            trigger();
-            measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
-            distance_cm = measure / 2.0 * sound_speed / 10.0;
-            delay(200);
-        }
-        while ((randNumber == 1) && (distance_cm <= 50)) {
-            turn_left();
-            randNumber = random(5, 20);
-            delay(randNumber);
-            stop_moving();
-            trigger();
-            measure = pulseIn(ECHO_PIN, HIGH, timeout_measure);
-            distance_cm = measure / 2.0 * sound_speed / 10.0;
-            delay(200);
-        }
-    } else
-        move_ahead();
+    distance = (measure / 2.0 * sound_speed / 10.0);
+
+    if (print_error && Serial && wait_delay < 200) {
+        print_error = !print_error;
+        Serial.print("get_distance() : You are under minimum required delay !\n");
+        Serial.print("get_distance() : Expecting unexpected issues :(\n\n");
+    }
+    delay(wait_delay);
+
+    return distance;
+}
+
+void wait_for_distance(float distance)
+{
+    while (get_distance() <= distance) {};
+}
+
+void turn_until_distance(float distance, void (*direction)())
+{
+    (*direction)();
+    wait_for_distance(distance);
+    stop_moving();
+}
+
+void auto_move()
+{
+    move_ahead();
+    if (get_distance() <= MIN_DISTANCE) {
+        if (random(2))
+            turn_until_distance(MIN_DISTANCE, &turn_right);
+        else
+            turn_until_distance(MIN_DISTANCE, &turn_left);
+    }
+    stop_moving();
 }
